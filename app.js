6ffbcb5ce858ee98ee95b242b3bb31cc6fcc0f03
{{nielchaudhary}} <119964539+nielchaudhary@users.noreply.github.com>
2024-03-28 17:08:32 +0530

    completed the controller

--- app.js
+++ app.js
@@ -8,6 +8,30 @@ app.use(cookieParser())
 
 
 
+const uniqueVisitors = new Set()
+function generator() {
+    const randomString  = Math.random().toString(36)
+    return `${randomString}`
+}
+
+app.use((req, res, next) => {
+    const visitorId = req.cookies["visitor_id"]
+    if (!visitorId) {
+        const newVisitorId = generator()
+        uniqueVisitors.add(newVisitorId)
+    }
+    next()
+
+})
+
+app.get('/unique-visitors', (req,res)=>{
+    res.json({count : uniqueVisitors.size})
+})
+
+
+app.listen(3000,()=>{
+    console.log("This server is running on Port 3000")
+})
 
 
 
